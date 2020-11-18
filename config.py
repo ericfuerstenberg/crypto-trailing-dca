231a3a91fb735eeb4df62b1d08114585ca0ad4fb
Eric Fuerstenberg <ericfuerstenberg@gmail.com>
2020-11-17 22:50:42 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Obtain API key at https://www.binance.com/userCenter/createApi.html
 
 API_DETAILS = {
-    'API_KEY': '4257b9c1cbc259881bd125c304a247dd',
-    'API_SECRET': 'NHD/6IcphKtXtnacvbZG5Lj09x1WKERt7i1gB+RyU1SlFJnE/gcGNuwSHIFK+PyWBT42eSOMe5+dwJjmYJ+RjA==',
-    'PASSWORD': 'mi512yem6al'
+    'API_KEY': <API_KEY>,
+    'API_SECRET': <API_SECRET>,
+    'PASSWORD': <PASSPHRASE>
 }
 
 EXIT_STRATEGY = {
