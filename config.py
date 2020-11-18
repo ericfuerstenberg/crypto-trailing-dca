062b90067b9a62aaee252cf8beb4a93d9b2a67e7
Eri Fuerstenberg <ericfuerstenberg@gmail.com>
2020-11-18 08:14:46 -0800

    adding logic to only initialze stoploss once our first threshold is hit

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # Obtain API key at https://www.binance.com/userCenter/createApi.html
 
 API_DETAILS = {
-    'API_KEY': '4257b9c1cbc259881bd125c304a247dd',
-    'API_SECRET': 'NHD/6IcphKtXtnacvbZG5Lj09x1WKERt7i1gB+RyU1SlFJnE/gcGNuwSHIFK+PyWBT42eSOMe5+dwJjmYJ+RjA==',
-    'PASSWORD': 'mi512yem6al'
+    'API_KEY': <API_KEY>,
+    'API_SECRET': <API_SECRET>,
+    'PASSWORD': <PASSPHRASE>
 }
 
 EXIT_STRATEGY = {
     'DATA': {
-        'price': [17791, 17833, 17840], 
+        'price': [17590, 17685, 17780], 
         'amount': [0.05, 0.09, 0.1]
     }
 }
