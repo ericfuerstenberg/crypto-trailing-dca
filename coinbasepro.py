afa26dd475870348ef9e5a648cb7ae64c51a5ddc
Eri Fuerstenberg <ericfuerstenberg@gmail.com>
2020-11-17 22:45:14 -0800

    adding hopper functionality

--- coinbasepro.py
+++ coinbasepro.py
@@ -5,11 +5,15 @@ import ccxt
 
 class CoinbasePro(): # CoinbasePro
 
-    def __init__(self, api_key, api_secret):
+    def __init__(self, api_key, api_secret, password):
         self.ccxtClient = ccxt.coinbasepro({ # ccxt.coinbasepro()
             'apiKey': api_key,
             'secret': api_secret, # verify that you only need these two credentials - maybe need password too?
+            'password': password
         })
+
+        # set sandbox mode
+        self.ccxtClient.set_sandbox_mode(True)
 
     def buy(self, market, amount, price):
         return (self.ccxtClient.create_order(
@@ -30,7 +34,7 @@ class CoinbasePro(): # CoinbasePro
         ))
 
     def get_price(self, market):
-        return float(self.ccxtClient.fetch_ticker(market)['info']['lastPrice'])
+        return float(self.ccxtClient.fetch_ticker(market)['info']['price'])
 
     def get_balance(self, coin):
         return float(self.ccxtClient.fetch_balance()[coin]['free'])
--- config.py
+++ config.py
@@ -1,6 +1,14 @@
 # Obtain API key at https://www.binance.com/userCenter/createApi.html
 
 API_DETAILS = {
-    'API_KEY': 'ENTER API KEY HERE',
-    'API_SECRET': 'ENTER API SECRET HERE'
+    'API_KEY': '***REMOVED***',
+    'API_SECRET': '***REMOVED***',
+    'PASSWORD': '***REMOVED***'
+}
+
+EXIT_STRATEGY = {
+    'DATA': {
+        'price': [17791, 17833, 17840], 
+        'amount': [0.05, 0.09, 0.1]
+    }
 }
