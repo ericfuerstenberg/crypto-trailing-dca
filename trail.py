1ae8ddd39ef18b1920170bc9a444ca2efcc14e51
Eri Fuerstenberg <ericfuerstenberg@gmail.com>
2020-11-18 16:43:57 -0800

    only initialize stoploss once first threshold is hit

--- trail.py
+++ trail.py
@@ -10,15 +10,12 @@ import pandas as pd
 # 1. DONE? - Instead of usin a static amount of funds, create a function to read from a dataframe, check against current price, and update a "holding pen/hopper" with more ETH as new thresholds are crossed (https://stackoverflow.com/questions/42285806/how-to-pop-rows-from-a-dataframe)
 # 2. DONE - Modify the stop loss to use a % rather than a static amount. E.g., 5% stop loss.
 # 3. DONE? - Modify the logging output to specify how much is currently in the hopper (e.g., ETH to trade: 0.5, 0.75, etc.)
-# 4. Adjust script so it only initializes the stop loss once a threshold is first hit
+# 4. DONE - Adjust script so it only initializes the stop loss once a threshold is first hit
 # 5. Set up actual logging output to a logfile. Print timestamps for each message. Hopper updates, stop loss updates, etc should all be logged separately for tracking purposes.
 # 6. Error handling? e.g., ccxt.base.errors.InsufficientFunds: coinbasepro Insufficient funds
 # 7. Prepare to dockerize the script
 
 class StopTrail():
-
-	print('starting StopTrail')
-	stoploss_initialized = False # can't get this variable to update from functions below. Why? I know it's not looping back up here.. it's just not updating below.
 
 	def __init__(self, market, type, stopsize, interval):
 		self.coinbasepro = CoinbasePro(
@@ -32,32 +29,27 @@ class StopTrail():
 		self.interval = interval
 		self.running = False
 		#self.stopinit = StopTrail.stoploss_initialized
-		print('Init: ' + str(StopTrail.stoploss_initialized))
+		#print('Init: ' + str(self.stoploss_initialized))
 		self.df, self.hopper = self.initialize_hopper()
+		self.stoploss_initialized = False
+		print('Init: ' + str(self.stoploss_initialized))
 
 	def initialize_stop(self):
-		global stoploss_initialized
-		#global self.stopinit
+		self.stoploss_initialized = True
 		price = self.coinbasepro.get_price(self.market)
-		stoploss_initialized = True
-		print('initialize stop status: ' + str(StopTrail.stoploss_initialized))
+		#print('initialize stop status: ' + str(self.stoploss_initialized))
 		if self.type == "buy":
 			self.stoploss = (price + (price * self.stopsize))
 			print('Stop loss initialized at: ' + str(self.stoploss))
-			return self.stoploss, stoploss_initialized
+			return self.stoploss, self.stoploss_initialized
 		else:
 			self.stoploss = (price - (price * self.stopsize))
 			print('Stop loss initialized at: ' + str(self.stoploss))
-			return self.stoploss, stoploss_initialized
-		# print('Stop loss initialized at: ' + str(self.stoploss))
-		# stoploss_initialized = True
-		# print('initialize stop status 2: ' + str(stoploss_initialized))
-		# return stoploss_initialized
-		# self.stopinit = True
-		# return self.stopinit
+			return self.stoploss, self.stoploss_initialized
 
 	def update_stop(self):
-		if StopTrail.stoploss_initialized is True:
+		#print('update_stop() :' + str(self.stoploss_initialized))
+		if self.stoploss_initialized is True:
 			price = self.coinbasepro.get_price(self.market)
 			if self.type == "sell":
 				if (price - (price * self.stopsize)) > self.stoploss:
@@ -91,7 +83,6 @@ class StopTrail():
 		self.hopper = 0
 		return df, self.hopper
 
-
 	def update_hopper(self):
 		# create a function that watches the current price and if it meets the threshold set in the dataframe, move the specified amount of ETH from the dataframe to the hopper
 		# self.update_hopper()
@@ -110,8 +101,8 @@ class StopTrail():
 				amount = first_row.iloc[0]['amount']
 				threshold = first_row.iloc[0]['price']
 				print('Hit our threshold at ' + str(threshold) + '. Adding ' + str(amount) + ' to hopper.')
-				print('Update Hopper stoploss status: ' + str(StopTrail.stoploss_initialized) )
-				if StopTrail.stoploss_initialized == False:
+				#print('Update Hopper stoploss status: ' + str(self.stoploss_initialized) )
+				if self.stoploss_initialized == False:
 					 self.initialize_stop()
 				self.hopper += amount
 				print('Hopper: ' + str(self.hopper))
@@ -123,34 +114,24 @@ class StopTrail():
 
 		return self.hopper
 
-
 	def print_status(self):
 		price = self.coinbasepro.get_price(self.market)
 		print("---------------------")
 		print("Trail type: %s" % self.type)
 		print("Market: %s" % self.market)
 		print("Available to sell: %s" % self.hopper)
-		print("Last price: %.8f" % price)
-		if StopTrail.stoploss_initialized is True:
+		if self.stoploss_initialized is True:
 			print("Stop loss: %s" % self.stoploss)
 		else:
 			print('Stop loss: N/A')
 		print("Trailing stop: %s percent" % (self.stopsize*100))
+		print("Last price: %.8f" % price)
 		print("---------------------")
-
 
 	def run(self):
 		self.running = True
 		while (self.running):
 			self.print_status()
-			self.update_hopper()
 			self.update_stop()
+			self.update_hopper()
 			time.sleep(self.interval)
-
-	# def run_once(f):
-	# 	def wrapper(*args, **kwargs):
-	# 		if not wrapper.has_run:
-	# 			wrapper.has_run = True
-	# 			return f(*args, **kwargs)
-	# 	wrapper.has_run = False
-	# 	return wrapper
