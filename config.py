9ebf128077a093e23ad73782951c07b6fcd23a69
Eric Fuerstenberg <efuerstenberg@box.com>
2020-11-19 11:11:39 -0800

    small tweaks

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # Obtain API key at https://www.binance.com/userCenter/createApi.html
 
 API_DETAILS = {
-    'API_KEY': <API_KEY>,
-    'API_SECRET': <API_SECRET>,
-    'PASSWORD': <PASSPHRASE>
+    'API_KEY': '<API_KEY>',
+    'API_SECRET': '<API_SECRET>',
+    'PASSWORD': '<PASSPHRASE>'
 }
 
-EXIT_STRATEGY = {
-    'DATA': {
-        'price': [17590, 17685, 17780], 
-        'amount': [0.05, 0.09, 0.1]
-    }
-}
+# EXIT_STRATEGY = {
+#     'DATA': {
+#         'price': [17590, 17685, 17780], 
+#         'amount': [0.05, 0.09, 0.1]
+#     }
+# }
