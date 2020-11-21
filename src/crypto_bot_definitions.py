65007494b97aa0623de32b96aa50791516daccee
Eric Fuerstenberg <efuerstenberg@box.com>
2020-11-21 11:10:00 -0800

    tweaks to definitions.py

--- src/crypto_bot_definitions.py
+++ src/crypto_bot_definitions.py
@@ -1,14 +1,9 @@
 import os
 
 #settings for running locally
-#ROOT_DIR = "/".join(os.path.dirname(os.path.join(__file__)).split("/")[:-1]) 
-#ROOT_DIR = os.path.join( os.path.dirname( __file__ ), '..' )
 ROOT_DIR = os.path.join( os.path.dirname ( __file__), os.path.pardir)
-print(ROOT_DIR)
 SRC_DIR = ROOT_DIR + "/src"
-print(SRC_DIR)
 CONF_DIR = ROOT_DIR + "/conf"
-print(CONF_DIR)
 LOG_DIR = ROOT_DIR + "/log"
 RESOURCES_DIR = ROOT_DIR + "/resources"
 
