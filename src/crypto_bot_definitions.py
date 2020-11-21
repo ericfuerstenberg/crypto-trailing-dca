2dd81840b401ed36769b6a8b67439b6622404145
Eric Fuerstenberg <efuerstenberg@box.com>
2020-11-21 11:08:23 -0800

    refactoring folder structure, modding how settings are picked up

--- src/crypto_bot_definitions.py
+++ src/crypto_bot_definitions.py
@@ -1,10 +1,14 @@
 import os
 
 #settings for running locally
-#ROOT_DIR = "/".join(os.path.dirname(os.path.join(__file__))#.split("/")[:-1]) 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+#ROOT_DIR = "/".join(os.path.dirname(os.path.join(__file__)).split("/")[:-1]) 
+#ROOT_DIR = os.path.join( os.path.dirname( __file__ ), '..' )
+ROOT_DIR = os.path.join( os.path.dirname ( __file__), os.path.pardir)
+print(ROOT_DIR)
 SRC_DIR = ROOT_DIR + "/src"
+print(SRC_DIR)
 CONF_DIR = ROOT_DIR + "/conf"
+print(CONF_DIR)
 LOG_DIR = ROOT_DIR + "/log"
 RESOURCES_DIR = ROOT_DIR + "/resources"
 
