919c6416b3f0947e879d658a770aa49a03e63e03
Eri Fuerstenberg <ericfuerstenberg@gmail.com>
2020-12-19 08:53:03 -0800

    updating init message

--- src/trail.py
+++ src/trail.py
@@ -64,6 +64,11 @@ class StopTrail():
 		self.running = False
 		self.tracked_price = self.coinbasepro.get_price(self.market)
 		self.tracked_balance = self.coinbasepro.get_balance(self.market.split("/")[1])
+
+		if self.split == 1:
+			logger.warning('Running in single coin mode: %s' % self.market)
+		else:
+			logger.warning('Running in multiple coin mode (%s)' % self.split)
 		
 		# open db connection and check for a persisted stoploss value
 		self.con = sl.connect("exit_strategy.db")
